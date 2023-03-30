6b7e6a2de471083109fa3727b30ddc7eac997493
Daniel Silva Ferreira Bruno <dsfbstudent@yahoo.com.br>
2023-03-30 18:08:41 -0300

    Implement the first version of the model and forms of the adopetapp app.

--- adopet/adopetproj/adopetapp/models.py
+++ adopet/adopetproj/adopetapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Tutor(models.Model):
+    email = models.EmailField()
+    name = models.CharField("Nome", max_length=384)
+    password = models.CharField("Senha", max_length=50)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name + ", " + self.email
